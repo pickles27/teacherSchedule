2ac39e3176520aee8c8ea176f430847368ebf191
Sarah Pickrahn <spickrahn@gmail.com>
2019-04-26 19:04:17 -0700

    changed table column names from camel case to snake case

--- scripts/database-setup.sql
+++ scripts/database-setup.sql
@@ -1,18 +1,18 @@
 CREATE DATABASE teacher_schedule_db;
 \c teacher_schedule_db;
 
-CREATE TABLE teachers (teacherId SERIAL PRIMARY KEY, 
-											 teacherName VARCHAR(100) NOT NULL);
+CREATE TABLE teachers (id SERIAL PRIMARY KEY, 
+											 teacher_name VARCHAR(100) NOT NULL);
 
-CREATE TABLE classes (classId SERIAL PRIMARY KEY, 
-											teacherId INTEGER REFERENCES teachers(teacherId),
-											className VARCHAR(100) NOT NULL);
+CREATE TABLE classes (id SERIAL PRIMARY KEY, 
+											teacher_id INTEGER REFERENCES teachers(id),
+											class_name VARCHAR(100) NOT NULL);
 
 CREATE TABLE schedule_entry (id SERIAL PRIMARY KEY,
-														 teacher INTEGER REFERENCES teachers(teacherId),
-														 class INTEGER REFERENCES classes(classId),
+														 teacher_id INTEGER REFERENCES teachers(id),
+														 class_id INTEGER REFERENCES classes(id),
 														 day VARCHAR(10) NOT NULL,
 														 hour INTEGER NOT NULL,
 														 minute INTEGER NOT NULL);
 
-INSERT INTO teachers (teacherName) VALUES ('Sally Jones');
+INSERT INTO teachers (teacher_name) VALUES ('Sally Jones');
