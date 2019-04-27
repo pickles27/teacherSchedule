3b216f8207100bd87dc87b79231e16e1953b5f16
Sarah Pickrahn <spickrahn@gmail.com>
2019-04-26 18:46:29 -0700

    teachers, classes and schedule entries tables created

--- scripts/database-setup.sql
+++ scripts/database-setup.sql
@@ -1,2 +1,18 @@
 CREATE DATABASE teacher_schedule_db;
+\c teacher_schedule_db;
 
+CREATE TABLE teachers (teacherId SERIAL PRIMARY KEY, 
+											 teacherName VARCHAR(100) NOT NULL);
+
+CREATE TABLE classes (classId SERIAL PRIMARY KEY, 
+											teacherId INTEGER REFERENCES teachers(teacherId),
+											className VARCHAR(100) NOT NULL);
+
+CREATE TABLE schedule_entry (id SERIAL PRIMARY KEY,
+														 teacher INTEGER REFERENCES teachers(teacherId),
+														 class INTEGER REFERENCES classes(classId),
+														 day VARCHAR(10) NOT NULL,
+														 hour INTEGER NOT NULL,
+														 minute INTEGER NOT NULL);
+
+INSERT INTO teachers (teacherName) VALUES ('Sally Jones');
